6bfc16d61a31168e80f1ba82bd02ca3234dc11c8
Jakob A <jdanders1999@gmail.com>
2023-04-03 00:18:13 -0400

    Set up mutation for saveBook within resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,39 +7,50 @@ const { signToken } = require("../utils/auth");
 
 // Set up queries for 'me' and data errors
 const resolvers = {
-    Query: {
-      me: async (parent, args, context) => {
-        if (context.user) {
-          const userData = await User.findOne({ _id: context.user._id }).select(
-            "-__v -password"
-          );
-          return userData;
-        }
-        throw new AuthenticationError("Not logged in");
-      },
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
     },
-    Mutation: {
-        login: async (parent, { email, password }) => {
-          const user = await User.findOne({ email });
-          // checking if user exists with email and credentials
-          if (!user) {
-            //   if not throw an error
-            throw new AuthenticationError("Invalid credentials");
-          }
-    
-          const correctPassword = await user.isCorrectPassword(password);
-          if (!correctPassword) {
-            throw new AuthenticationError("Invalid credentials");
-          }
-          const token = signToken(user);
-    
-          return { token, user };
-        },
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-      
-            return { token, user };
-          },
-    }
-}
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      // checking if user exists with email and credentials
+      if (!user) {
+        //   if not throw an error
+        throw new AuthenticationError("Invalid credentials");
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+      if (!correctPassword) {
+        throw new AuthenticationError("Invalid credentials");
+      }
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};

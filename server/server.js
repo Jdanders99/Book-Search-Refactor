ead5d7d1d0294792f458847ddfb1d17680240416
Jakob A <jdanders1999@gmail.com>
2023-04-03 00:42:01 -0400

    Rearranged some things in server.js to use await server.start

--- server/server.js
+++ server/server.js
@@ -14,11 +14,9 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => req.user,
-  introspection: true,
-  playground: true,
 });
 
+await server.start();
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
@@ -29,8 +27,20 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+
+  startApolloServer(typeDefs, resolvers);

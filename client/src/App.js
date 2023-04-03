fb39bd466b1ce94452ed2146bbfdc28b1c8e18ee
Jakob A <jdanders1999@gmail.com>
2023-04-02 23:18:58 -0400

    Set up new Apollo client for link and cache within App.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,8 +20,14 @@ const authLink = setContext((_, { headers }) => {
     };
 });
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), 
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -32,6 +38,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 

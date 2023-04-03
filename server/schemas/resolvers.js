f32200bad8b0be85496c33473c75a4d2d72391b6
Jakob A <jdanders1999@gmail.com>
2023-04-03 00:13:01 -0400

    Set up query within resolvers variable

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,3 +4,18 @@ const { AuthenticationError } = require("apollo-server-errors");
 const { User } = require("../models");
 // importing signToken from utils
 const { signToken } = require("../utils/auth");
+
+// Set up queries for 'me' and data errors
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id }).select(
+            "-__v -password"
+          );
+          return userData;
+        }
+        throw new AuthenticationError("Not logged in");
+      },
+    },
+}
